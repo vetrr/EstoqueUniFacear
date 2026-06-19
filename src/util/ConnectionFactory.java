8d52f0a6af26bd93330bb2004524c208296100ed
Vitor Hugo De Lima Moreira <vitor.moreira@UNIFACEAR.ADM>
2026-06-18 21:45:35 -0300

    conectividade com o banco de dados

---
+++ src/util/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    protected static String url = "jdbc:postgresql://10.20.2.64:5432/EstoqueUniFacear";
+    protected static String usuario = "estoque";
+    protected static String senha = "Unifacear@2026";
+
+    // 2. O método que as outras classes vão chamar para pegar a conexão
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, usuario, senha);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("A conexão falhou: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args){
+        getConnection();
+    }
+
+}
